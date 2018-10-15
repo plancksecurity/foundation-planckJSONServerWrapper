07989fd419a13a0b6ae0faf53a9d8cf6aa305d5f
Roker <roker@pep-project.org>
2018-10-15 16:42:53 +0200

    remove undo_last_mistrust(), because it is no longer in the Engine.

--- server/ev_server.cc
+++ server/ev_server.cc
@@ -140,7 +140,6 @@ const FunctionMap functions = {
 		FP( "own_key_is_listed", new Func<PEP_STATUS, In_Pep_Session, In<c_string>, Out<bool>> ( &own_key_is_listed) ),
 		FP( "own_identities_retrieve", new Func<PEP_STATUS, In_Pep_Session, Out<identity_list*>>( &own_identities_retrieve ) ),
 		FP( "set_own_key", new Func<PEP_STATUS, In_Pep_Session, InOut<pEp_identity*>, In<c_string>>( &set_own_key ) ),
-		FP( "undo_last_mistrust", new Func<PEP_STATUS, In_Pep_Session>( &undo_last_mistrust ) ),
 		
 		FP( "myself"        , new Func<PEP_STATUS, In_Pep_Session, InOut<pEp_identity*>> ( &myself) ),
 		FP( "update_identity", new Func<PEP_STATUS, In_Pep_Session, InOut<pEp_identity*>> ( &update_identity) ),
