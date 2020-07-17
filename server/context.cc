4e350d56cf16dbaaa16637959222157079ad43c1
Roker <roker@pep-project.org>
2020-07-17 14:20:18 +0200

    add debug output for base64 codec and c_string length storing & retreiving.

--- server/context.cc
+++ server/context.cc
@@ -1,13 +1,33 @@
 #include "context.hh"
+#include "logger.hh"
+
+namespace
+{
+	Logger& Log()
+	{
+		static Logger L("ctx");
+		return L;
+	}
+}
+
 
 void Context::store(int position, size_t value)
 {
+	DEBUG_OUT( Log(), "Store value %zu for position %d.", value, position);
 	obj_store.emplace( position, value );
 }
 
 size_t Context::retrieve(int position)
 {
-	return obj_store.at(position);
+	try{
+		const size_t value = obj_store.at(position);
+		DEBUG_OUT( Log(), "Retrieve value %zu for position %d.", value, position);
+		return value;
+	}catch(const std::out_of_range& e)
+	{
+		Log() << Logger::Error << "There is no value stored at position " << position << "!";
+		throw;
+	}
 }
 
 
