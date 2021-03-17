cb33137c85e9012f52f6277d6f817ad607b20035
roker <roker@pep-project.org>
2021-03-17 17:18:59 +0100

    copy JS test files from old JSON-128 branch

---
+++ html/unittests.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// all unittests will add themselves here:
+var unittests =
+	[
+		{"description":"call gen_random_name()", "onclick":"random_name_test()" }
+	];
+
+
+var base32_alphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
+
+// creates a string of 12 random letters
+function gen_random_name()
+{
+	var rndArray = new Uint32Array(12);
+	window.crypto.getRandomValues(rndArray);
+	var s = '';
+	for(var i=0; i<rndArray.length; ++i)
+	{
+		s += base32_alphabet[ rndArray[i] % (base32_alphabet.length-1) ];
+	}
+	return s;
+}
+
+function add_test_buttons()
+{
+	var content = '<table><tr class="t"><th></th><th>Test name</th><th></th></tr>\n';
+	for(var i=0; i<unittests.length; ++i)
+	{
+		var u = unittests[i];
+		content += '<tr class="t"><td>#' + (i+1) + '</td><td>' + u.description + '</td>'
+			+ '<td><button type="button" onclick="' + u.onclick + '"> Run! </button></td></tr>\n';
+	}
+	content += '</table>';
+	document.getElementById('unittest_div').innerHTML = content;
+}
+
+function random_name_test()
+{
+	document.getElementById('unittest_out').innerHTML =  'pEp_' + gen_random_name() + '@pep.lol';
+}
