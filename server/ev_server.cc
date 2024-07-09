7e2b6cae93094f0106676964644d1c0220e4c4b2
Dirk Zimmermann <dz@planck.security>
2024-07-09 15:21:30 +0200

    CORE-54 cache_encrypt_message_with_full_input interface

--- server/ev_server.cc
+++ server/ev_server.cc
@@ -107,6 +107,7 @@ const FunctionMap functions = {
 		FP( "Message API", new Separator ),
 		FP( "encrypt_message", new FuncPC<PEP_STATUS, In_Pep_Session, InOut<message*>, In<stringlist_t*>, Out<message*>, In<PEP_enc_format>, In<PEP_encrypt_flags_t>>( &encrypt_message ) ),
 		FP( "cache_encrypt_message", new FuncPC<PEP_STATUS, In_Pep_Session, InOut<message*>, In<stringlist_t*>, Out<message*>, In<PEP_enc_format>, In<PEP_encrypt_flags_t>>( &pEp::MessageCache::cache_encrypt_message ) ),
+		FP( "cache_encrypt_message_with_full_input", new FuncPC<PEP_STATUS, In_Pep_Session, InOut<message*>, In<stringlist_t*>, Out<message*>, In<PEP_enc_format>, In<PEP_encrypt_flags_t>>( &pEp::MessageCache::cache_encrypt_message_with_full_input ) ),
 		FP( "encrypt_message_and_add_priv_key", new FuncPC<PEP_STATUS, In_Pep_Session,
 			In<message*>, Out<message*>, In<c_string>, In<PEP_enc_format>, In<PEP_encrypt_flags_t>>( &encrypt_message_and_add_priv_key) ),
 		FP( "encrypt_message_for_self", new FuncPC<PEP_STATUS, In_Pep_Session,
